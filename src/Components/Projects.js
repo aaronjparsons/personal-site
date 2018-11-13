fff8343b5b0e3bc33ee55574a59043d3d71faa59
aaronjparsons <aaron.parsons@live.ca>
2018-11-12 23:30:42 -0700

    added media queries for mobile responsiveness and keys to project list

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -46,9 +46,9 @@ class Projects extends Component {
       <div className='container'>
         <h1 style={{'color': `${this.props.background}`, 'transition': 'color 600ms'}}>PROJECTS:</h1>
         <div className='project-container' id='projects'>
-          {projects.map((project) => {
+          {projects.map((project, index) => {
             return (
-              <a href={`${project.link}`}>
+              <a href={`${project.link}`} key={index}>
                 <div className={`project-box ${project.class}`}>
                   <h1 className="text-default" style={{'backgroundColor': `${this.props.background}`, 'color': `${this.props.fontColor}`}}>{`${project.name}`}</h1>
                   <p className="text-hover" style={{'backgroundColor': `${this.props.background}`, 'color': `${this.props.fontColor}`}}>{`${project.desc}`}</p>
